21e4110b1bfc41d596f3ef6cdf995a6adc93be6c
Jyothi Marisetty <jyothi.marisetty@tcs.com>
2016-06-20 09:45:11 -0400

    console Logger class added

---
+++ ConsoleLog/ConsoleLog/ConsoleL.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleLog
+{
+    public class ConsoleL
+    {
+        public void WriteLine(String text)
+        {
+            Console.WriteLine(text);
+        }
+    }
+}
